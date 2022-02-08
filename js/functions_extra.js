c6fa14300e3a2f9d32c598a677a88d677379023d
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-02-08 10:19:25 -0600

    did more of function extra exercise

--- js/functions_extra.js
+++ js/functions_extra.js
@@ -66,3 +66,79 @@ function double(num){
 }
 
 console.log(double(prompt("type in a number an I will double it")));
+
+function half(n){
+    return n/2;
+}
+
+console.log(half(prompt("type a number an ill half it")))
+
+function triple(n){
+    return n*3;
+}
+
+console.log(triple(prompt("type a number an ill triple it")));
+
+function quadruple(n){
+    return n*4;
+}
+console.log(quadruple(prompt("type a number an ill quadruple it")));
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+console.log(subtract(prompt("Give me the first number to subtract"), prompt("give me the second")))
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+console.log(multiply(prompt("Give me the first number to multiply"), prompt("give me the second")))
+
+function isEven(number){
+    if (number % 2 === 0){
+        return number+ " is even"
+    }else{
+        return number+" is not even"
+    }
+}
+
+console.log(isEven(2))
+
+function isOdd(number){
+    if (number % 2 === 0){
+        return number+" is not Odd"
+    }else{
+        return number+" is Odd"
+    }
+}
+
+console.log(isOdd(3))
+
+function divide(num1, num2){
+    return num1/num2;
+}
+console.log(divide(prompt("Give me the first number to divide"), prompt("give me the second")))
+
+function isFive(number){
+    if (number == 5){
+        return number + " is 5 😎"
+    }else {
+        return  number + " is not 5 🙃"
+    }
+}
+
+console.log(isFive(prompt("type in the number 5")))
+
+function trim(string){
+    return string.trim();
+}
+
+console.log(trim(" hello "));
+
+function rollDice(sidesOfDice){
+    let luckyNumber = Math.floor(Math.random() * sidesOfDice)+1;
+    return "You rolled "+luckyNumber;
+}
+
+console.log(rollDice(prompt("how many sides the dice should have")))
