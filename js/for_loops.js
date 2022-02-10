8561ef5c7f1583578cb4ebbb5be1eccb66a5fe73
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-02-10 11:20:27 -0600

    Finshed For loop pratice

--- js/for_loops.js
+++ js/for_loops.js
@@ -14,3 +14,15 @@ for (var i = 1; i <= 10; i++){
         console.log(random + " is even");
     }
 }
+
+let number =1;
+//console.log(number)
+for (let i = number; i <=9; i++){
+    console.log(number)
+    number += ""+i;
+    //console.log(number);
+}
+
+for (var num = 100; num >= 5; num -= 5){
+    console.log(num);
+}
