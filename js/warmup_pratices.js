01d32578733ca744f092184f5b9663374eb09073
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-02-16 09:28:25 -0600

    did some praitces

--- js/warmup_pratices.js
+++ js/warmup_pratices.js
@@ -12,3 +12,25 @@ function findAverage(average){
     return sum / grade.length;
 }
 console.log(findAverage(grade));
+
+// Create an object representing a person. It should have properties representing:
+// names (an array of strings)
+// date of birth
+// occupation
+let person =[
+    {
+    name:["Robert","Smith"],
+    dateOfBirth: "05/09/1990",
+    occupation: "firefighter"
+    },
+
+    {
+        name: ["Joe","Swatson"],
+        dateOfBirth: "03/26/1987",
+        occupation: "Police"
+    }
+];
+console.log(person);
+console.log(person[0].name);
+console.log(person[0].dateOfBirth);
+console.log(person[0].occupation);
