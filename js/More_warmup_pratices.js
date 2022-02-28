d4793b88d2a6270ecac5be919fa6822fec1ac0b8
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-02-28 09:32:48 -0600

    pratices

---
+++ js/More_warmup_pratices.js
@@ -0,0 +1,23 @@
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+console.log(removeAll(bugs, "ant"));
+
+function removeAll(bugs, removeBugs){
+    let filterbugs =[];
+    bugs.forEach(function (ele){
+        if (removeBugs !== ele){
+            filterbugs.push(ele)
+        }
+    });
+    return filterbugs
+}
+    // return  bugs.indexOf(removeBugs);
+// should print
+// [
+//     'mosquito',
+//     'scorpion',
+//     'mosquito',
+//     'typo',
+//     'reference error',
+//     'type error'
+// ]
