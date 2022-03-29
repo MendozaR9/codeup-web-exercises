1e354a22c7fee12f6ae94b5701cd2badaf930aa4
Ryan Mendoza <ryanmendoza.00@gmail.com>
2022-03-29 11:44:13 -0500

    Get my recent commit and time

--- js/promises.js
+++ js/promises.js
@@ -1,11 +1,13 @@
-
-
-fetch(`https://api.github.com/users/MendozaR9/events`, {headers: { 'Authorization': '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'}})
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        console.log(data[0].payload.commits[0].message)
-    })
-    .catch(error => function (error){
-        console.log(error)
-    })
+getuserData()
+function getuserData() {
+    fetch(`https://api.github.com/users/MendozaR9/events`, {headers: {'Authorization': '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'}})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            console.log(data[0].created_at)
+            console.log(data[0].payload.commits[0].message)
+        })
+        .catch(error => function (error) {
+            console.log("u get error")
+        })
+}
